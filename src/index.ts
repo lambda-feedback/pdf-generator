4b630827f3ee05576975e31828dff7e3e8f1a392
Jarka <57811074+jarkabaker@users.noreply.github.com>
2024-03-21 12:03:41 +0000

    Change to code to run from AWS Lambda funtion

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello from PDF Generator. How are you today?')
+
+import { APIGatewayEvent, Context } from 'aws-lambda';
+
+console.log('Hello from PDF Generator. This is a test')
+
+export const handler = async function (event: APIGatewayEvent, context: Context) {
+  const queries = 
+  console.log('Processing event: %s', JSON.stringify(event, null, 2));
+  return {
+    statusCode: 200,
+    url: 'myURL'
+  }
+};
