c4a3aa2e2a62764f889260b0320bcd35895a5446
Jarka <57811074+jarkabaker@users.noreply.github.com>
2024-03-25 16:56:13 +0000

    added console log

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { APIGatewayEvent, Context } from 'aws-lambda';
 console.log('Hello from PDF Generator. This is a test')
 
 export const handler = async function (event: APIGatewayEvent, context: Context) {
+  console.error('Lambda function handler called')
   const queries = 
   console.log('Processing event: %s', JSON.stringify(event, null, 2));
   return {
