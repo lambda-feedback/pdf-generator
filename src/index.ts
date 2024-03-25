88a6dc2d5fc82cb6eee556fc6b980a1cc5530a60
Jarka <57811074+jarkabaker@users.noreply.github.com>
2024-03-25 17:45:13 +0000

    change

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { APIGatewayEvent, Context } from 'aws-lambda';
 
 export const handler = async function (event: APIGatewayEvent, context: Context) {
   console.error('Lambda function handler called')
-  const queries = 
   console.log('Processing event: %s', JSON.stringify(event, null, 2));
   return {
     statusCode: 200,
