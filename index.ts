9b9090fe5e4a567e32100d444c541aec2f27b096
Jarka <57811074+jarkabaker@users.noreply.github.com>
2024-03-27 15:39:28 +0000

    Added zod

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
 
 import { APIGatewayEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
+import * as z from 'zod';
+
+export const SetSchema = z.object({
+  functionName: z.string(),
+  dockerImageUri: z.string(),
+  apiKey: z.string(),
+});
 
 export const handler = async function (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> {
   console.error('Lambda function handler called')
-  console.log('Processing event: %s', JSON.stringify(event, null, 2));
+  const message = JSON.parse(JSON.stringify(event));
+  event.queryStringParameters
+  console.log('Processing this event:', message);
   return {
     statusCode: 200,
     body: JSON.stringify({
