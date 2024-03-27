0db0419d60bfee16dae7d2f82f5953cf3d3a72e7
Jarka <57811074+jarkabaker@users.noreply.github.com>
2024-03-27 07:37:03 +0000

    deployment changes

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ export const handler = async function (event: APIGatewayEvent, context: Context)
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'what a lovely day',
+      message: 'what a lovely day there',
     })
   }
 };
