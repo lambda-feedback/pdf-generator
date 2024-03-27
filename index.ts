c28edb00b5b3183f56d41c9592a92790ed5edd13
Jarka <57811074+jarkabaker@users.noreply.github.com>
2024-03-27 07:57:22 +0000

    Removed function uri variable

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ export const handler = async function (event: APIGatewayEvent, context: Context)
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'what a lovely day there',
+      message: 'what a lovely day there, is not it?',
     })
   }
 };
